da7648d8b1e9fea6c751537256b536fca2f03334
Plotkin <Plotkin2016@gmail.com>
2024-10-27 17:18:46 +0200

    small fix

--- lib/data.ts
+++ lib/data.ts
@@ -44,6 +44,7 @@ export const experiencesData = [
   },
   {
     title: 'WebUI Project-Based Learning',
+    location: '',
     description:
       "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
     icon: React.createElement(FaReact),
