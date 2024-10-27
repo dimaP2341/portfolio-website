55d6d1bcfb40d95e5e13ef9f59fbb6e6910fdacc
Plotkin <Plotkin2016@gmail.com>
2024-10-27 17:13:09 +0200

    added new photo and CV

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,40 +1,36 @@
-"use client";
+'use client'
 
-import Image from "next/image";
-import React from "react";
-import { motion } from "framer-motion";
-import Link from "next/link";
-import { BsArrowRight, BsLinkedin } from "react-icons/bs";
-import { HiDownload } from "react-icons/hi";
-import { FaGithubSquare } from "react-icons/fa";
-import useSectionInView from "@/hooks/useSectionInView";
-import { useActiveSectionContext } from "@/context/active-section-content";
+import Image from 'next/image'
+import React from 'react'
+import { motion } from 'framer-motion'
+import Link from 'next/link'
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs'
+import { HiDownload } from 'react-icons/hi'
+import { FaGithubSquare } from 'react-icons/fa'
+import useSectionInView from '@/hooks/useSectionInView'
+import { useActiveSectionContext } from '@/context/active-section-content'
 
 export default function Intro() {
-  const { ref } = useSectionInView("Home", 0.5);
-  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+  const { ref } = useSectionInView('Home', 0.5)
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext()
 
   return (
-    <section
-      ref={ref}
-      id='home'
-      className='mb-28 sm:mt-2 max-w-[50rem] text-center sm:mb-0 scroll-mt-[99rem]'
-    >
-      <div className='flex items-center justify-center'>
-        <div className='relative'>
+    <section ref={ref} id="home" className="mb-28 sm:mt-2 max-w-[50rem] text-center sm:mb-0 scroll-mt-[99rem]">
+      <div className="flex items-center justify-center">
+        <div className="relative">
           <motion.div
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
-            transition={{ type: "tween", duration: 0.4 }}
+            transition={{ type: 'tween', duration: 0.4 }}
           >
             <Image
-              width='192'
-              height='192'
-              quality='95'
-              src='/myPhoto.jpg'
-              alt='Dmytro Plotkin'
+              width="192"
+              height="192"
+              quality="95"
+              src="/myPhoto.jpg"
+              alt="Dmytro Plotkin"
               priority={true}
-              className='h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl'
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
             />
           </motion.div>
 
@@ -42,14 +38,14 @@ export default function Intro() {
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{
-              type: "spring",
+              type: 'spring',
               stiffness: 130,
               delay: 0.2,
               duration: 0.7,
             }}
-            className='absolute text-4xl bottom-0 -right-1'
+            className="absolute text-4xl bottom-0 -right-1"
           >
-            💖
+            😄
           </motion.span>
         </div>
       </div>
@@ -57,53 +53,50 @@ export default function Intro() {
       <motion.h1
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
-        className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-3xl text-justify indent-8'
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-3xl text-justify indent-8"
       >
-        <span className='font-bold'>Dmytro is a front-end developer</span> who
-        specializes in building user interfaces, ensuring responsive design, and
-        optimizing web performance to create seamless user experiences.
+        <span className="font-bold">Dmytro is a front-end developer</span> who specializes in building user interfaces,
+        ensuring responsive design, and optimizing web performance to create seamless user experiences.
       </motion.h1>
 
       <motion.div
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.1 }}
-        className='flex flex-col sm:flex-row items justify-center gap-2 px-4 text-lg font-medium'
+        className="flex flex-col sm:flex-row items justify-center gap-2 px-4 text-lg font-medium"
       >
         <Link
-          href='#contact'
-          className='bg-gray-900 group text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-105 hover:scale-105 hover:bg-gray-950 active:scale-100 transition'
+          href="#contact"
+          className="bg-gray-900 group text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-105 hover:scale-105 hover:bg-gray-950 active:scale-100 transition"
           onClick={() => {
-            setActiveSection("Contact");
-            setTimeOfLastClick(Date.now());
+            setActiveSection('Contact')
+            setTimeOfLastClick(Date.now())
           }}
         >
-          Contact me here{" "}
-          <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' />
+          Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
         <a
-          href='/CV.pdf'
+          href="/CV.pdf"
           download
-          className='bg-white group px-7 py-3 border border-black/10 flex items-center gap-2 rounded-full outline-none focus:scale-105 hover:scale-105 active:scale-100 transition cursor-pointer dark:bg-white/10 dark:text-white/60'
+          className="bg-white group px-7 py-3 border border-black/10 flex items-center gap-2 rounded-full outline-none focus:scale-105 hover:scale-105 active:scale-100 transition cursor-pointer dark:bg-white/10 dark:text-white/60"
         >
-          Download CV{" "}
-          <HiDownload className='opacity-60 group-hover:translate-y-1 transition' />
+          Download CV <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
         <a
-          target='_blank'
-          href='https://www.linkedin.com/in/dmytro-plotkin-44620b26b/'
-          className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-105 hover:scale-105 active:scale-100 transition border border-black/10 dark:bg-white/10 dark:text-white/60'
+          target="_blank"
+          href="https://www.linkedin.com/in/dmytro-plotkin-44620b26b/"
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-105 hover:scale-105 active:scale-100 transition border border-black/10 dark:bg-white/10 dark:text-white/60"
         >
           <BsLinkedin />
         </a>
         <a
-          target='_blank'
-          href='https://github.com/dimaP2341'
-          className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-105 hover:scale-105 active:scale-100 transition border border-black/10 dark:bg-white/10 dark:text-white/60'
+          target="_blank"
+          href="https://github.com/dimaP2341"
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-105 hover:scale-105 active:scale-100 transition border border-black/10 dark:bg-white/10 dark:text-white/60"
         >
           <FaGithubSquare />
         </a>
       </motion.div>
     </section>
-  );
+  )
 }
--- lib/data.ts
+++ lib/data.ts
@@ -1,107 +1,95 @@
-import React from "react";
-import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import React from 'react'
+import { CgWorkAlt } from 'react-icons/cg'
+import { FaReact } from 'react-icons/fa'
+import { LuGraduationCap } from 'react-icons/lu'
+import corpcommentImg from '@/public/corpcomment.png'
+import rmtdevImg from '@/public/rmtdev.png'
+import wordanalyticsImg from '@/public/wordanalytics.png'
 
 export const links = [
   {
-    name: "Home",
-    hash: "#home",
+    name: 'Home',
+    hash: '#home',
   },
   {
-    name: "About",
-    hash: "#about",
+    name: 'About',
+    hash: '#about',
   },
   {
-    name: "Projects",
-    hash: "#projects",
+    name: 'Projects',
+    hash: '#projects',
   },
   {
-    name: "Skills",
-    hash: "#skills",
+    name: 'Skills',
+    hash: '#skills',
   },
   {
-    name: "Experience",
-    hash: "#experience",
+    name: 'Experience',
+    hash: '#experience',
   },
   {
-    name: "Contact",
-    hash: "#contact",
+    name: 'Contact',
+    hash: '#contact',
   },
-] as const;
+] as const
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: 'Kharkiv National University of Radio Electronics',
+    location: 'Kharkiv, Ukraine',
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      'Kharkiv National University of Radio Electronics, NURE, is one of the most distinctly profiled universities in Ukraine, where applied IT and innovation for sustainable are in focus.  NURE has the vision to conduct education and research in which engineering and IT can be integrated with other disciplines. Everything we do at NURE has three distinct perspectives: innovation, sustainability and in real life, which means collaboration and exchange with both the business and industry as well as society.',
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: '2023-2027',
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
-    description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-    icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
-  },
-  {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
+    title: 'WebUI Project-Based Learning',
     description:
       "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: '2024 - present',
   },
-] as const;
+] as const
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: 'CorpComment',
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+      'I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.',
+    tags: ['React', 'Next.js', 'MongoDB', 'Tailwind', 'Prisma'],
     imageUrl: corpcommentImg,
   },
   {
-    title: "rmtDev",
+    title: 'rmtDev',
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+      'Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.',
+    tags: ['React', 'TypeScript', 'Next.js', 'Tailwind', 'Redux'],
     imageUrl: rmtdevImg,
   },
   {
-    title: "Word Analytics",
+    title: 'Word Analytics',
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+      'A public web app for quick analytics on text. It shows word count, character count and social media post limits.',
+    tags: ['React', 'Next.js', 'SQL', 'Tailwind', 'Framer'],
     imageUrl: wordanalyticsImg,
   },
-] as const;
+] as const
 
 export const skillsData = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "TypeScript",
-  "React",
-  "Next.js",
-  "Node.js",
-  "Git",
-  "Tailwind",
-  "Prisma",
-  "MongoDB",
-  "Redux",
-  "GraphQL",
-  "Apollo",
-  "Express",
-  "PostgreSQL",
-  "Python",
-  "Django",
-  "Framer Motion",
-] as const;
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Next.js',
+  'Node.js',
+  'Git',
+  'GitHub',
+  'Tailwind',
+  'Prisma',
+  'MongoDB',
+  'Redux',
+  'Express',
+  'PostgreSQL',
+  'Framer Motion',
+] as const
