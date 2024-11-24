cf6bc7acee12c5046f506936803cd3d6744f470b
Plotkin <Plotkin2016@gmail.com>
2024-11-24 21:35:47 +0200

    changed some data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,36 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/header";
-import ActiveSectionContent from "@/context/active-section-content";
-import { Toaster } from "react-hot-toast";
-import Footer from "@/components/footer";
-import ThemeSwitch from "@/components/theme-switch";
-import ThemeContextProvider from "@/context/theme-context";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Header from '@/components/header'
+import ActiveSectionContent from '@/context/active-section-content'
+import { Toaster } from 'react-hot-toast'
+import Footer from '@/components/footer'
+import ThemeSwitch from '@/components/theme-switch'
+import ThemeContextProvider from '@/context/theme-context'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Dmytro Plotkin | Portfolio",
+  title: 'Dmytro Plotkin | Portfolio',
   description:
-    "Dmytro is a front-end developer who specializes in building user interfaces, ensuring responsive design, and optimizing web performance to create seamless user experiences.",
-};
+    'Dmytro is a front-end developer who specializes in building user interfaces, ensuring responsive design, and optimizing web performance to create seamless user experiences.',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang='en' className='!scroll-smooth'>
+    <html lang="en" className="!scroll-smooth">
       <head>
-        <link rel='icon' href='/icon.png' type='image/png' sizes='32px' />
+        <link rel="icon" href="/icon.png" type="image/png" sizes="32px" />
       </head>
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-35 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
-        <div className='bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.3rem] w-[31.3rem] rounded-full blur-[10rem] sm:w-[67rem] -z-50 dark:bg-[#946263]'></div>
-        <div className='bg-[rgb(219,215,251)] absolute top-[-1rem] left-[-35rem] h-[31.3rem] w-[41.3rem] rounded-full blur-[10rem] sm:w-[67rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] -z-50 dark:bg-[#676394]'></div>
+        <div className="bg-[#fbe2f6] absolute top-[-6rem] right-[11rem] h-[31.3rem] w-[31.3rem] rounded-full blur-[10rem] sm:w-[67rem] -z-50 dark:bg-[#946263]"></div>
+        <div className="bg-[rgb(219,215,251)] absolute top-[-1rem] left-[-35rem] h-[31.3rem] w-[41.3rem] rounded-full blur-[10rem] sm:w-[67rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] -z-50 dark:bg-[#676394]"></div>
 
         <ThemeContextProvider>
           <ActiveSectionContent>
@@ -38,10 +38,10 @@ export default function RootLayout({
             {children}
             <Footer />
             <ThemeSwitch />
-            <Toaster position='top-right' />
+            <Toaster position="top-right" />
           </ActiveSectionContent>
         </ThemeContextProvider>
       </body>
     </html>
-  );
+  )
 }
--- lib/data.ts
+++ lib/data.ts
@@ -48,29 +48,28 @@ export const experiencesData = [
     description:
       "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
     icon: React.createElement(FaReact),
-    date: '2024 - present',
+    date: '2024',
   },
 ] as const
 
 export const projectsData = [
   {
     title: 'CorpComment',
-    description:
-      'I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.',
+    description: 'A personal project where I built a platform for users to give public feedback to companies.',
     tags: ['React', 'Next.js', 'MongoDB', 'Tailwind', 'Prisma'],
     imageUrl: corpcommentImg,
   },
   {
     title: 'rmtDev',
     description:
-      'Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.',
+      'A pet project designed as a job board for remote developer jobs, with filtering, sorting, and pagination features. Focused on front-end development.',
     tags: ['React', 'TypeScript', 'Next.js', 'Tailwind', 'Redux'],
     imageUrl: rmtdevImg,
   },
   {
     title: 'Word Analytics',
     description:
-      'A public web app for quick analytics on text. It shows word count, character count and social media post limits.',
+      'A personal web app for quick text analytics, including word and character counts and social media post limits. Built to explore web development concepts.',
     tags: ['React', 'Next.js', 'SQL', 'Tailwind', 'Framer'],
     imageUrl: wordanalyticsImg,
   },
